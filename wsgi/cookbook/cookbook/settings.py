bb47186ab98e1990cb0d12596223dbf8bfc6e085
Mateusz Bocian <matbocian91@gmail.com>
2016-02-04 20:40:04 -0500

    Fixing build error with secret import

--- wsgi/cookbook/cookbook/settings.py
+++ wsgi/cookbook/cookbook/settings.py
@@ -1,3 +1,10 @@
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
+
+import os
+import sys
+from socket import gethostname
+
 """
 Django settings for cookbook project.
 
@@ -9,35 +16,29 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
 DJ_PROJECT_DIR = os.path.dirname(__file__)
 BASE_DIR = os.path.dirname(DJ_PROJECT_DIR)
 WSGI_DIR = os.path.dirname(BASE_DIR)
 REPO_DIR = os.path.dirname(WSGI_DIR)
 DATA_DIR = os.environ.get('OPENSHIFT_DATA_DIR', BASE_DIR)
 
-import sys
 sys.path.append(os.path.join(REPO_DIR, 'libs'))
+
+# Application secret keys
 import secrets
 SECRETS = secrets.getter(os.path.join(DATA_DIR, 'secrets.json'))
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = SECRETS['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-from socket import gethostname
 ALLOWED_HOSTS = [
     gethostname(),  # For internal OpenShift load balancer security purposes.
-    os.environ.get('OPENSHIFT_APP_DNS'),  # Dynamically map to the OpenShift gear name.
+    os.environ.get('OPENSHIFT_APP_DNS'),  # OpenShift gear name.
     # 'example.com', # First DNS alias (set up in the app)
     # 'www.example.com', # Second DNS alias (set up in the app)
 ]
-
 
 # Application definition
 
