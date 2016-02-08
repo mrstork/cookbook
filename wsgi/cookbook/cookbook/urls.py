7c043e2ea4e7641321f50841ad81bd76ab5ae771
Mateusz Bocian <matbocian91@gmail.com>
2016-02-07 23:18:34 -0500

    Adding in recipe related starting points

--- wsgi/cookbook/cookbook/urls.py
+++ wsgi/cookbook/cookbook/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^', include('public.urls')),
     url(r'^accounts/', include('accounts.urls')),
+    url(r'^recipes/', include('recipes.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- wsgi/cookbook/recipes/views.py
+++ wsgi/cookbook/recipes/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def list_view(request):
+    context = {}
+    return render(request, 'list-recipes.html', context)
+
+
+def add_view(request):
+    context = {}
+    return render(request, 'edit-recipe.html', context)
+
+
+def edit_view(request):
+    context = {}
+    return render(request, 'edit-recipe.html', context)
+
+
+def detail_view(request):
+    context = {}
+    return render(request, 'view-recipe.html', context)
