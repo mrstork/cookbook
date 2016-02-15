047f9fb3e715762a9a907a7a2fb98f11584cb93a
Mateusz Bocian <matbocian91@gmail.com>
2016-02-15 14:39:16 -0500

    Adding login/logout links to template

--- wsgi/cookbook/accounts/urls.py
+++ wsgi/cookbook/accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^request_account$', views.request_account, name='request_account'),
     url(r'^create_account/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.confirm_account, name='confirm_account'),
+    url(r'^logout$', views.logout_view, name='logout'),
 ]
