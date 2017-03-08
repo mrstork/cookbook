eeb6be9a4191a9e0afb16de081f4c841071320d4
Mateusz Bocian <matbocian91@gmail.com>
2017-03-07 19:32:52 -0500

    Adjusting site urls on recipe pages

--- wsgi/cookbook/recipes/urls.py
+++ wsgi/cookbook/recipes/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^add/$', views.add_view, name='add_recipe'),
-    url(r'^(?P<user>.+)/(?P<slug>.+)/view$', views.detail_view, name='view_recipe'),
-    url(r'^(?P<user>.+)/(?P<slug>.+)/edit$', views.edit_view, name='edit_recipe'),
-    url(r'^(?P<user>.+)(/)?$', views.list_view, name='list_recipes'),
+    url(r'^create/$', views.add_view, name='create-recipe'),
+    url(r'^(?P<user>.+)/(?P<slug>.+)/view$', views.detail_view, name='view-recipe'),
+    url(r'^(?P<user>.+)/(?P<slug>.+)/edit$', views.edit_view, name='edit-recipe'),
+    url(r'^(?P<user>.+)(/)?$', views.list_view, name='list-recipes'),
 ]
