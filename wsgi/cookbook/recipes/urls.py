2baf5672bd68da28c7a065c8bee148ff7f2abc5d
Mateusz Bocian <matbocian91@gmail.com>
2017-03-07 19:00:01 -0500

    Adding optional slash to the end of recipe list page

--- wsgi/cookbook/recipes/urls.py
+++ wsgi/cookbook/recipes/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^add/$', views.add_view, name='add_recipe'),
     url(r'^(?P<user>.+)/(?P<slug>.+)/view$', views.detail_view, name='view_recipe'),
     url(r'^(?P<user>.+)/(?P<slug>.+)/edit$', views.edit_view, name='edit_recipe'),
-    url(r'^(?P<user>.+)$', views.list_view, name='list_recipes'),
+    url(r'^(?P<user>.+)(/)?$', views.list_view, name='list_recipes'),
 ]
