efddf49da661ebf3926b2a4d024d734306dd8ae6
Mateusz Bocian <matbocian91@gmail.com>
2017-03-28 20:51:26 -0400

    Sending image data and recipe data seperately

--- wsgi/cookbook/public/static/app.js
+++ wsgi/cookbook/public/static/app.js
@@ -7,9 +7,6 @@
       // Allows form submission with django csrf protection
       $httpProvider.defaults.xsrfCookieName = 'csrftoken';
       $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
-      // Allows form to send files
-      $httpProvider.defaults.headers.post['Content-Type'] = undefined;
-      $httpProvider.defaults.transformRequest = angular.identity;
     },
   ]);
 
@@ -89,17 +86,7 @@
       this.recipe.ingredients = this.ingredients;
       this.recipe.equipment = this.equipment;
       this.recipe.instructions = this.instructions;
-
-      if (cropper) {
-        var _this = this;
-        cropper.getCroppedCanvas().toBlob(function (blob) {
-          blob.name = Date.now() + '.png';
-          _this.recipe.image = blob;
-          _this.post();
-        }, 'image/png');
-      } else {
-        this.post();
-      }
+      this.post();
     };
 
     this.publish = function () {
@@ -109,213 +96,47 @@
 
     // TODO: Watch for changes and post periodically
     this.post = function () {
+      var _this = this;
       var url = window.location.href;
-      var formdata = Object.toFormData(this.recipe);
-      if (!this.recipe.image || typeof this.recipe.image === 'string') {
-        formdata.delete('image');
-      }
-      $http.post(url, formdata)
+      var data = Object.assign({}, this.recipe);
+      delete data.image;
+
+      $http.post(url, data)
+      .then(function () {
+        if (cropper) {
+          _this.postImage();
+        } else {
+          window.location = '/recipes'
+        }
+      }, function () {
+        // TODO: display a readable error
+        console.log('error');
+      });
+    };
+
+    this.postImage = function () {
+      var _this = this;
+      cropper.getCroppedCanvas().toBlob(function (blob) {
+        var url = window.location.href;
+        var filename = Date.now() + '.png';
+        var formdata = new FormData();
+        formdata.append('id', _this.recipe.id);
+        formdata.append('user', _this.recipe.user);
+        formdata.append('image', blob, filename);
+        $http.post(url, formdata, {
+          // Allows angular to send files
+          headers: {
+            'Content-Type': undefined,
+          },
+          transformRequest: angular.identity,
+        })
         .then(function () {
           window.location = '/recipes'
         }, function () {
           // TODO: display a readable error
         });
-    }
+      }, 'image/png');
+    };
 
   });
 })();
-
-// TODO: Remove all this extra crap below, and build your own abridged version
-
-/*
- object-to-formdata v0.1.2
- https://github.com/nervgh/object-to-formdata
-*/
-(function(window) {
-    'use strict';
-
-    var Blob = window.Blob;
-    var File = window.File;
-    var FileList = window.FileList;
-    var FormData = window.FormData;
-
-    var isSupported = (Blob && File && FileList && FormData);
-    var toString = Object.prototype.toString;
-    var forEach = Array.prototype.forEach;
-    var map = Array.prototype.map;
-
-    if (!isSupported) return;
-
-    /**
-     * Returns type of anything
-     * @param {Object} any
-     * @returns {String}
-     */
-    function getType(any) {
-        return toString.call(any).slice(8, -1);
-    }
-    /**
-     * Converts path to FormData name
-     * @param {Array} path
-     * @returns {String}
-     */
-    function toName(path) {
-        var array = map.call(path, function(value) {
-            return '[' + value + ']';
-        });
-        array[0] = path[0];
-        return array.join('');
-    }
-
-    /**
-     * Converts object to FormData
-     * @param {Object} object
-     * @returns {FormData}
-     */
-    function toFormData(object) {
-        var form = new FormData();
-        var cb = function(node, value, key, path) {
-            var type = getType(value);
-
-            switch (type) {
-                case 'Array':
-                    break; // step into
-                case 'Object':
-                    break; // step into
-                case 'FileList':
-                    forEach.call(value, function(item, index) {
-                        var way = path.concat(index);
-                        var name = toName(way);
-                        form.append(name, item);
-                    });
-                    return true; // prevent step into
-                case 'File':
-                    var name = toName(path);
-                    form.append(name, value);
-                    return true; // prevent step into
-                case 'Blob':
-                    var name = toName(path);
-                    form.append(name, value, value.name);
-                    return true; // prevent step into
-                default:
-                    var name = toName(path);
-                    form.append(name, value);
-                    return true; // prevent step into
-            }
-        };
-
-        Object.traverse(object, cb, null, null, true);
-
-        return form;
-    }
-
-    // export
-    Object.toFormData = toFormData;
-
-}(window));
-
-
-/*
- object-traverse v0.1.1
- https://github.com/nervgh/object-traverse
-*/
-(function(window) {
-    'use strict';
-
-    var MAX_DEPTH = 100;
-    var getKeys = Object.keys;
-    var isNaN = window.isNaN;
-    /**
-     * Returns "true" if any is object
-     * @param {*} any
-     * @returns {Boolean}
-     */
-    function isObject(any) {
-        return any instanceof Object;
-    }
-    /**
-     * Returns "true" if any is number
-     * @param {*} any
-     * @returns {Boolean}
-     */
-    function isNumber(any) {
-        return typeof any === 'number' && !isNaN(any);
-    }
-    /**
-     * Walks object recursively
-     * @param {Object} object
-     * @param {Function} cb
-     * @param {*} ctx
-     * @param {Boolean} mode
-     * @param {Boolean} ignore
-     * @param {Number} max
-     * @returns {Object}
-     */
-    function walk(object, cb, ctx, mode, ignore, max) {
-        var stack = [[], 0, getKeys(object).sort(), object];
-        var cache = [];
-
-        do {
-            var node = stack.pop();
-            var keys = stack.pop();
-            var depth = stack.pop();
-            var path = stack.pop();
-
-            cache.push(node);
-
-            while(keys[0]) {
-                var key = keys.shift();
-                var value = node[key];
-                var way = path.concat(key);
-                var strategy = cb.call(ctx, node, value, key, way, depth);
-
-                if (strategy === true) {
-                    continue;
-                } else if (strategy === false) {
-                    stack.length = 0;
-                    break;
-                } else {
-                    if(max <= depth || !isObject(value)) continue;
-
-                    if (cache.indexOf(value) !== -1) {
-                        if (ignore) continue;
-                        throw new Error('Circular reference');
-                    }
-
-                    if (mode) {
-                        stack.unshift(way, depth + 1, getKeys(value).sort(), value);
-                    } else {
-                        stack.push(path, depth, keys, node);
-                        stack.push(way, depth + 1, getKeys(value).sort(), value);
-                        break;
-                    }
-                }
-            }
-        } while(stack[0]);
-
-        return object;
-    }
-    /**
-     * Walks object recursively
-     * @param {Object} object
-     * @param {Function} callback
-     * @param {*} [context]
-     * @param {Number} [mode=0]
-     * @param {Boolean} [ignoreCircularReferences=false]
-     * @param {Number} [maxDepth=100]
-     * @returns {Object}
-     */
-    function traverse(object, callback, context, mode, ignoreCircularReferences, maxDepth) {
-        var cb = callback;
-        var ctx = context;
-        var _mode = mode === 1;
-        var ignore = !!ignoreCircularReferences;
-        var max = isNumber(maxDepth) ? maxDepth : MAX_DEPTH;
-
-        return walk(object, cb, ctx, _mode, ignore, max);
-    }
-
-    // export
-    Object.traverse = traverse;
-
-}(window));
