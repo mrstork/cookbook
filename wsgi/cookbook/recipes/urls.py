c9bfc0c3bf1dd6820d580d25c58cb03be3c73ce8
Mateusz Bocian <matbocian91@gmail.com>
2017-03-29 18:23:13 -0400

    Minor tweaks to routing

--- wsgi/cookbook/recipes/urls.py
+++ wsgi/cookbook/recipes/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^create$', views.add_view, name='create-recipe'),
     url(r'^(?P<user>.+)/(?P<pk>[0-9]+)$', views.RecipeDetail.as_view(), name='recipe-detail'),
     url(r'^(?P<user>.+)(/)?$', views.RecipeList.as_view(), name='recipe-list'),
-    url(r'^$', views.base_view),
+    url(r'^$', views.base_view, name='all-recipes'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
