cf2d50238afd22a45cfb2ef228acfd3c678d10d3
Mateusz Bocian <matbocian91@gmail.com>
2017-03-31 15:13:07 -0400

    Removing first/last name from registration confirmation

--- wsgi/cookbook/accounts/forms.py
+++ wsgi/cookbook/accounts/forms.py
@@ -49,17 +49,11 @@ class RequestAccountForm(forms.Form):
 class ConfirmAccountForm(forms.Form):
     username = forms.CharField(label='Username', max_length=100,
                                  widget=forms.TextInput(attrs={'required': 'true', 'autofocus': ''}))
-    first_name = forms.CharField(label='First Name', max_length=100,
-                                 widget=forms.TextInput(attrs={'required': 'true'}))
-    last_name = forms.CharField(label='Last Name', max_length=100,
-                                widget=forms.TextInput(attrs={'required': 'true'}))
     password = forms.CharField(label='Password',
                                widget=forms.PasswordInput(attrs={'required': 'true'}))
 
     def confirm_account(self, user):
         user.username = self.cleaned_data['username']
-        user.first_name = self.cleaned_data['first_name']
-        user.last_name = self.cleaned_data['last_name']
         user.set_password(self.cleaned_data['password'])
         user.is_active = True
         user.save()
