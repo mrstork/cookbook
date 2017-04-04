86489366c425e29c3ad430e29834b6d6a529bdbf
Mateusz Bocian <matbocian91@gmail.com>
2017-04-04 16:11:24 -0400

    Added request account tests

--- wsgi/cookbook/accounts/tests.py
+++ wsgi/cookbook/accounts/tests.py
@@ -1,6 +1,5 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 from django.core import mail
-
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 
@@ -12,40 +11,49 @@ def create_user(email, password):
     user.save()
     authenticate(username=email, password=password)
 
-# request account but cannot sign in
 
+class TestRequestAccount(TestCase):
 
-class TestAccountRequestEmail(TestCase):
+    def test_request_account(self):
+        test_email = 'test@test.com'
+        client = Client()
+        response = client.post('/accounts/request_account', {'email': test_email})
+        self.assertEqual(response.status_code, 200)
 
-    def setUp(self):
-        mail.outbox = []
-        pass
+        # Registration email sent
+        self.assertEqual(len(mail.outbox), 1)
 
-    def test_seding_email(self):
-        pass
+        # User added to the database
+        self.assertEqual(User.objects.count(), 1)
 
-    def tearDown(self):
-        mail.outbox = []
-
-
-class TestAccountRequest(TestCase):
-
-    def setUp(self):
-        # create two users
-        pass
-
-    def test_incorrect_user(self):
-        pass
-
-    def test_incorrect_key(self):
-        pass
-
-    def test_user_key_mismatch(self):
-        pass
-
-    def test_happy_path(self):
-        pass
+        # New user cannot login
+        user = User.objects.get(id=1)
+        self.assertEqual(user.email, test_email)
+        self.assertFalse(user.is_active)
 
     def tearDown(self):
-        # delete all users
-        pass
+        mail.outbox = []
+        User.objects.all().delete()
+
+
+# class TestValidateAccount(TestCase):
+#
+#     def setUp(self):
+#         # create two users
+#         pass
+#
+#     def test_incorrect_user(self):
+#         pass
+#
+#     def test_incorrect_key(self):
+#         pass
+#
+#     def test_user_key_mismatch(self):
+#         pass
+#
+#     def test_happy_path(self):
+#         pass
+#
+#     def tearDown(self):
+#         mail.outbox = []
+#         User.objects.all().delete()
