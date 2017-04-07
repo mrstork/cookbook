fe1d1d3c2b5ed88e88f51f90afdeb9effd825270
Mateusz Bocian <matbocian91@gmail.com>
2017-04-07 02:21:54 -0400

    Now that django version updated on server this import should work properly

--- wsgi/cookbook/accounts/fields.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-import unicodedata
-
-class UsernameField(forms.CharField):
-    def to_python(self, value):
-        return unicodedata.normalize('NFKC', super().to_python(value))
