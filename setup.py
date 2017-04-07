840ad397cbda53a603cbd143b93b0086d680fed3
Mateusz Bocian <matbocian91@gmail.com>
2017-04-07 02:07:34 -0400

    Updating setup file with new info and upping django version

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup
 
 setup(
-    name='kukkubu',
+    name='Ryorisho',
     version='1.0',
-    description='A place to store your personal recipes',
+    description='Share recipes and discover new ones',
     author='Mateusz Bocian',
-    author_email='support@kukkubu.com',
+    author_email='support@ryorisho.com',
     url='http://mrstork.github.io/',
     install_requires=[
-        'Django==1.8.4'
+        'Django==1.11'
     ],
     dependency_links=[
         'https://pypi.python.org/simple/django/'
