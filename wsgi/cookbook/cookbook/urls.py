213a62b2acb02313f9c25622fdaf9bfe66efa457
Mateusz Bocian <matbocian91@gmail.com>
2017-04-07 17:31:25 -0400

    Removing function and placing url directly in urlpatterns

--- wsgi/cookbook/cookbook/urls.py
+++ wsgi/cookbook/cookbook/urls.py
@@ -4,16 +4,11 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.static import serve
 
-def static(prefix, view=serve, **kwargs):
-    return [
-        url(r'^%s(?P<path>.*)$' % re.escape(prefix.lstrip('/')), view, kwargs=kwargs),
-    ]
-
 urlpatterns = [
     url(r'^', include('public.urls')),
     url(r'^accounts/', include('accounts.urls')),
     url(r'^recipes/', include('recipes.urls')),
     url(r'^admin/', include(admin.site.urls)),
     # /health, /env used by openshift
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+    url(r'^%s(?P<path>.*)$' % re.escape(settings.MEDIA_URL.lstrip('/')), serve, {'document_root': settings.MEDIA_ROOT}),
 ]
