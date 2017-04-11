95ea2ce4cfa5e6cc9927cb42f4ac97c28486dd2b
Mateusz Bocian <matbocian91@gmail.com>
2017-04-11 03:42:51 -0400

    Improving image optimization script by using pngquant

--- .openshift/cron/daily/optimize-recipe-images.py
+++ .openshift/cron/daily/optimize-recipe-images.py
@@ -1,18 +1,17 @@
 import os
-from PIL import Image
+import pngquant
 
 CRON_DAILY_DIR = os.path.dirname(os.path.abspath(__file__))
 CRON_DIR = os.path.dirname(CRON_DAILY_DIR)
 OPENSHIFT_DIR = os.path.dirname(CRON_DIR)
 REPO_DIR = os.path.dirname(OPENSHIFT_DIR)
 
-recipe_images_directory = os.path.join(REPO_DIR, 'wsgi', 'static', 'media','recipe-images')
+if os.environ.get('OPENSHIFT_APP_DNS'):
+    recipe_images_directory = os.path.join(REPO_DIR, 'wsgi', 'static', 'media','recipe-images')
+else:
+    recipe_images_directory = os.path.join(REPO_DIR, 'wsgi', 'media','recipe-images')
 
 for filename in os.listdir(recipe_images_directory):
     absolute_filepath = os.path.join(recipe_images_directory, filename)
-    image = Image.open(absolute_filepath)
-    image.save(absolute_filepath, optimize=True)
-
-# TODO: Images could probably be smaller with another library since pillow doesnt seem to have the best optimization
-# https://pypi.python.org/pypi/picopt/
-# https://pypi.python.org/pypi/pngquant/1.0.5
+    pngquant.config(absolute_filepath)
+    pngquant.quant_image(absolute_filepath)
