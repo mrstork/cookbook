470357d41be9f6ef645da5c8c1401f4a4f05e29c
Mateusz Bocian <matbocian91@gmail.com>
2017-06-28 01:19:44 -0400

    Verification numbers changed

--- wsgi/cookbook/public/views.py
+++ wsgi/cookbook/public/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def letsencrypt_verification(request):
-    return HttpResponse("Q5SduN6E7TdVdOf-5QctkLAUKfMpBcOGWU9uoe4PGrQ.XL-z7MVJQ86Sm96eh66a1JG9NoLw9X1CW9OVBdHzK7E")
+    return HttpResponse("Ue5gFdAxr7B5JiMqbvqz5wLCwWwW6BSBK9z2P1WRW_Q.XL-z7MVJQ86Sm96eh66a1JG9NoLw9X1CW9OVBdHzK7E")
 
 def index(request):
     return render(request, 'index.html')
