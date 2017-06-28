200c70fed30ca3d87a7af96716827c3778c71b6d
Mateusz Bocian <matbocian91@gmail.com>
2017-06-28 01:17:42 -0400

    Adding letsencrypt verification url

--- wsgi/cookbook/public/urls.py
+++ wsgi/cookbook/public/urls.py
@@ -5,6 +5,7 @@ from django.views.generic import RedirectView
 from public import views
 
 urlpatterns = [
+    url(r'.well-known/acme-challenge/Q5SduN6E7TdVdOf-5QctkLAUKfMpBcOGWU9uoe4PGrQ^$', views.letsencrypt_verification),
     url(r'^favicon\.ico$', RedirectView.as_view(url='%sfavicon.ico' % settings.STATIC_URL, permanent=True)),
     url(r'^$', views.index, name='landing-page'),
 ]
--- wsgi/cookbook/public/views.py
+++ wsgi/cookbook/public/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+def letsencrypt_verification(request):
+    return HttpResponse("Q5SduN6E7TdVdOf-5QctkLAUKfMpBcOGWU9uoe4PGrQ.XL-z7MVJQ86Sm96eh66a1JG9NoLw9X1CW9OVBdHzK7E")
+
 def index(request):
     return render(request, 'index.html')
